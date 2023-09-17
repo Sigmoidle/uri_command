2850a6e829fda2a7f776f43fd10069a418b0a5a7
Sigmoidle <ben.jordanoubailey@btinternet.com>
2023-09-17 18:21:13 +0100

    First commit

---
+++ uri_command/uri_command/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace uri_command;
+
+public abstract class Hello
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "obsidian://new?vault=Personal%20Vault&name=NewFile&append",
+            UseShellExecute = true
+        });
+        
+        Console.WriteLine("Done now :)");
+    }
+}
+
+
